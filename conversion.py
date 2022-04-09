29d85e0152a80a090f48cc17c01035bd90e0dbf1
saifrahmn <saifrahmn78@gmail.com>
2022-04-09 11:46:37 +0530

    update

---
+++ conversion.py
@@ -0,0 +1,6 @@
+sig1,sig2 = input(print("Enter Σ \n")).split()
+states=int(input(print("Enter number of states")))
+q = []
+for i in range(0,states):
+    q[i]= input("Enter name of the states")
+print(q)
