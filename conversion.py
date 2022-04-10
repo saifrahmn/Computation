c9d5439378e1aa457898a8cffb21ef42473e2488
saifrahmn <saifrahmn78@gmail.com>
2022-04-10 20:43:52 +0530

    nfa to dfa

--- conversion.py
+++ conversion.py
@@ -4,3 +4,9 @@ q = []
 for i in range(0,states):
     q[i]= input("Enter name of the states")
 print(q)
+
+for i in range(q):
+    if q = something that never came before:
+       # Make changes accordingly in nfa
+    else:
+        #check other states transition function
